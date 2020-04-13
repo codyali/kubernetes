95069c14be9713e5dec44b10bb2fb289089d1d30
Liyakat Ali <liyakat.ali@qantas.com.au>
2020-04-14 09:56:10 +1000

    updating client

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ docker push iyakatsahil/multi-client:$SHA
 docker push iyakatsahil/multi-server:$SHA
 docker push iyakatsahil/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=liyakatsahil/multi-server:$SHA
-kubectl set image deployments/client-deployments client=liyakatsahil/multi-client:$SHA
-kubectl set image deployments/worker-deployments worker=liyakatsahil/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=liyakatsahil/multi-server:$SHA
+kubectl set image deployments/client-deployment client=liyakatsahil/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=liyakatsahil/multi-worker:$SHA
